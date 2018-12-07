c9a5d316ce99684650d2c3d526565c75695b5470
Administrator <Administrator@CHINA-20181102R>
2018-12-07 17:17:55 +0800

    77

--- src/main/java/com/yidu/hhh.java
+++ src/main/java/com/yidu/hhh.java
@@ -1,7 +1,7 @@
 package com.yidu;
 
 public class hhh {
-public static void main(String[] args) {
-	System.out.println(132);
-}
+	public static void main(String[] args) {
+		System.out.println(132);
+	}
 }
